8ea288f5d7e97abe8128efc1435136152e5c1120
Fuhai Li <55822442+mslfh@users.noreply.github.com>
2025-05-20 13:50:48 +1000

    fix: Restore execution of curl request in sendSms method for proper SMS sending functionality

--- app/Repositories/SmsRepository.php
+++ app/Repositories/SmsRepository.php
@@ -53,8 +53,8 @@ class SmsRepository implements SmsContract
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true); // Enable SSL verification
         curl_setopt($ch, CURLOPT_CAINFO, storage_path('certificates/cacert.pem')); // Path to the CA certificate file
 
-        // $result = curl_exec($ch);
-        $result =  "{\"meta\":{\"code\":200,\"status\":\"SUCCESS\"},\"msg\":\"Queued\",\"data\":{\"messages\":[{\"message_id\":\"5D64C2FD-D68E-96E5-0D34-3ED1A3AD327C\",\"from\":\"61481076130\",\"to\":\"61491928668\",\"body\":\"Hello, this is a test message\",\"date\":\"2025-04-29 14:09:39\",\"custom_string\":\"\",\"direction\":\"out\"}],\"total_numbers\":1,\"success_number\":1,\"credits_used\":1},\"low_sms_alert\":\"Your account credits are low, you have 29.00 credits remaining, please top-up via the platform\"}";
+        $result = curl_exec($ch);
+        // $result =  "{\"meta\":{\"code\":200,\"status\":\"SUCCESS\"},\"msg\":\"Queued\",\"data\":{\"messages\":[{\"message_id\":\"5D64C2FD-D68E-96E5-0D34-3ED1A3AD327C\",\"from\":\"61481076130\",\"to\":\"61491928668\",\"body\":\"Hello, this is a test message\",\"date\":\"2025-04-29 14:09:39\",\"custom_string\":\"\",\"direction\":\"out\"}],\"total_numbers\":1,\"success_number\":1,\"credits_used\":1},\"low_sms_alert\":\"Your account credits are low, you have 29.00 credits remaining, please top-up via the platform\"}";
 
         curl_close($ch);
         return json_decode($result);
